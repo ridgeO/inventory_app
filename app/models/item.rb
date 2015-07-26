49dcbe04b35355a1e974f8791de68e34b80cfc80
ridge <ridge.olivieri@gmail.com>
2015-07-27 08:43:13 +0900

    image upload debug

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ActiveRecord::Base
   # Validates the size of an uploaded picture
   def picture_size
     if picture.size > 5.megabytes
-      errors.add(:picture, "should be less than 5MB")
+      errors.add(:image, "should be less than 5MB")
     end
   end
 
