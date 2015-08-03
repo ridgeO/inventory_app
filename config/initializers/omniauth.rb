87533f4907f66dd4c3b3e422356b337f6d3fd524
ridge <ridge.olivieri@gmail.com>
2015-08-03 17:24:56 -0400

    change oauth config file debug

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],{
-    prompt: "select account",
+    prompt: "select_account",
     scope: "profile",
     image_aspect_ration: "square",
     image_size: 48,
