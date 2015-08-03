ccd5c41cb62c3e87dc5c4b120ad9d5755d3f40a0
ridge <ridge.olivieri@gmail.com>
2015-08-03 17:01:04 -0400

    change locations home. omniauth altered

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
-    scope: "profile", image_aspect_ration: "square", image_size: 48, access_type: "online"
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],{
+    prompt: "select account",
+    scope: "profile",
+    image_aspect_ration: "square",
+    image_size: 48,
+    access_type: "online"
+  }
 end
