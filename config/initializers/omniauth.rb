8a04467cec98a8799048ede62404abd43b801bc3
ridge <ridge.olivieri@gmail.com>
2015-08-03 17:59:09 -0400

    fix omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
   {
-    :scope => "email,profile"
+    :scope => "email,profile",
+    :image_aspect_ratio => "square",
+    :image_size => 50,
+    :access_type => "offline"
   }
 end
